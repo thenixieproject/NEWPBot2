2eece6cf665817cf09d3562625784dc3cf4633e3
Quinn McFarland <lilmcfarlands@gmail.com>
2026-03-20 21:37:39 -0700

    Moved everything over to the JetBrains IDE which is cross-platform and better for me.

---
+++ main.cpp
@@ -0,0 +1,11 @@
+// Library Inclusions
+// Header Inclusions
+#include "cmdrClass.h"
+// Global Constants
+// Global Variables
+// Function Prototypes
+// Main Function
+int main() {
+	return 0;
+}
+// Function Definitions
