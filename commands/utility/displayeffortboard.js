6b29195c10ad84597197d13f24fb9d5c49d6ccb1
Quinn McFarland <lilmcfarlands@gmail.com>
2026-04-04 11:04:10 -0700

    Added ASCII-Table 3 as a dependency

--- commands/utility/displayeffortboard.js
+++ commands/utility/displayeffortboard.js
@@ -1,1 +1,16 @@
+// Module Imports
+const { SlashCommandBuilder, TextDisplayBuilder, MessageFlags } = require('discord.js');
+const { activeBgsSystems, activePowerplaySystems } = require('/Users/quinnmcfarland/Repos/NEWPBot2/effortdata.js');
 
+// Variables
+let bgsDisplay;
+let powerplayDisplay;
+
+// Helper Functions
+async function displayTableBuilder(startingArray, endingString) {
+
+}
+
+// Text Display Component
+
+// Command Execution
